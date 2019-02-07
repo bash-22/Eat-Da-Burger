aafef04c4043c5465f798a0f3dc59d82ffea8c91
bechirSafina <sfinabechir@gmail.com>
2019-02-07 04:01:12 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('McChicken', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Crab-cake burger', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Jack Burger', false);
